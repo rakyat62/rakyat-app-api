bd9786a25a9cb196f88bfedbd221072d9d31ffef
wkwksama <mailpakgasing@gmail.com>
2019-12-29 22:00:50 +0700

    setup deploy gitlab

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+// pm2 config
+module.exports = {
+  apps: [{
+    name: 'rakyat-app-api',
+    script: 'npm',
+    args: 'start',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }],
+};

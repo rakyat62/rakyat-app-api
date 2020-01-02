262935417cba1500913db2e80d6ab75dcd3af94c
wkwksama <mailpakgasing@gmail.com>
2020-01-02 18:56:17 +0700

    fix(create incident): created with auth

--- src/schema/incident/incident.resolvers.js
+++ src/schema/incident/incident.resolvers.js
@@ -1,23 +1,41 @@
-import { Incident } from '../../models';
+import models from '../../models';
 import pubsub from '../../utils/pubsub';
+import { verifyToken } from '../../utils/auth';
 
 export default {
   Query: {
     incidents: async () => {
-      const incidents = await Incident.find();
+      const incidents = await models.Incident.findAll();
       return incidents;
     },
+    incidentLabels: () => models.IncidentLabel.findAll(),
   },
   Mutation: {
-    createIncident: async (parent, { data }) => {
-      const incident = await Incident.create(data);
-      pubsub.publish('NEW_INCIDENT', { newIncident: incident });
-      return incident;
+    createIncident: async (parent, { input }, { request }) => {
+      const auth = verifyToken(request);
+      const user = await models.User.findByPk(auth.id);
+      if (!user) throw Error('User not found. Please re-login');
+
+      const payload = {
+        ...input,
+        status: 'OPEN',
+        createdBy: user.id,
+      };
+
+      const newIncident = await models.Incident.create(payload);
+
+      pubsub.publish('NEW_INCIDENT', { newIncident });
+      return newIncident;
     },
   },
   Subscription: {
     newIncident: {
       subscribe: () => pubsub.asyncIterator('NEW_INCIDENT'),
     },
+  },
+
+  Incident: {
+    createdBy: (parent) => models.User.findByPk(parent.createdBy),
+    label: (parent) => models.IncidentLabel.findByPk(parent.label),
   },
 };

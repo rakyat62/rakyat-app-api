2c9ec115b14017fae6c41a30c84bb8ccf2149c0d
wkwksama <mailpakgasing@gmail.com>
2020-01-09 02:02:29 +0700

    add cross origin allow

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ server.start({
   port: process.env.PORT || 4000,
   cors: {
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://rakyat62.netlify.com'],
   },
 }, (info) => {
   console.log('The server is up!', info);

5f90650416d807555dee725a6e13cd9e8395bb24
wkwksama <mailpakgasing@gmail.com>
2020-01-17 00:28:39 +0700

    seed incidents fake

---
+++ src/models/seeders/20200116165136-fake-incidents.js
@@ -0,0 +1,35 @@
+const faker = require('faker');
+const moment = require('moment');
+
+const dateNow = moment().format('YYYY-MM-DD HH:mm');
+
+const getRandomNumber = (from, to, fixed = 0) => {
+  const num = Math.random() * (to - from) + from;
+  return num.toFixed(fixed) * 1;
+};
+
+
+const getRandomStatus = () => {
+  const statuses = ['OPEN', 'CLOSED'];
+  return statuses[getRandomNumber(0, 1)];
+};
+
+
+const data = new Array(1000).fill({}).map(() => ({
+  information: faker.random.words(getRandomNumber(5, 40)),
+  status: getRandomStatus(),
+  locationAddress: faker.address.city(),
+  locationLat: getRandomNumber(-7, -6, 7),
+  locationLng: getRandomNumber(110, 111, 7),
+  images: '["https://res.cloudinary.com/sharofuddin/image/upload/v1579108469/rakyat62/td5aeumusxl8bnmqs8zu.jpg","https://res.cloudinary.com/sharofuddin/image/upload/v1579108469/rakyat62/gpedzxq63u5rvxhyppr1.png","https://res.cloudinary.com/sharofuddin/image/upload/v1579108469/rakyat62/gar88dckzsxctumjxxba.png"]',
+  createdBy: getRandomNumber(4, 11),
+  label: getRandomNumber(1, 3),
+  createdAt: moment().year(2019).month(getRandomNumber(1, 12)).date(getRandomNumber(1, 28))
+    .format('YYYY-MM-DD HH:mm'),
+  updatedAt: dateNow,
+}));
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('incidents', data, {}),
+
+  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('incidents', null, {}),
+};

ce69dda975bb4b09dd85d80b526d3b5579792165
wkwksama <mailpakgasing@gmail.com>
2020-01-17 01:24:26 +0700

    fix faker incidents

--- src/models/seeders/20200116165136-fake-incidents.js
+++ src/models/seeders/20200116165136-fake-incidents.js
@@ -15,16 +15,16 @@ const getRandomStatus = () => {
 };
 
 
-const data = new Array(1000).fill({}).map(() => ({
-  information: faker.random.words(getRandomNumber(5, 40)),
+const data = new Array(200).fill({}).map(() => ({
+  information: faker.random.words(getRandomNumber(15, 40)),
   status: getRandomStatus(),
   locationAddress: faker.address.city(),
-  locationLat: getRandomNumber(-7, -6, 7),
+  locationLat: getRandomNumber(-8, -7, 7),
   locationLng: getRandomNumber(110, 111, 7),
   images: '["https://res.cloudinary.com/sharofuddin/image/upload/v1579108469/rakyat62/td5aeumusxl8bnmqs8zu.jpg","https://res.cloudinary.com/sharofuddin/image/upload/v1579108469/rakyat62/gpedzxq63u5rvxhyppr1.png","https://res.cloudinary.com/sharofuddin/image/upload/v1579108469/rakyat62/gar88dckzsxctumjxxba.png"]',
   createdBy: getRandomNumber(4, 11),
   label: getRandomNumber(1, 3),
-  createdAt: moment().year(2019).month(getRandomNumber(1, 12)).date(getRandomNumber(1, 28))
+  createdAt: moment().year(2019).month(getRandomNumber(8, 12)).date(getRandomNumber(1, 28))
     .format('YYYY-MM-DD HH:mm'),
   updatedAt: dateNow,
 }));

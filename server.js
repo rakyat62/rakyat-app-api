9ed9183b96f3da51fb2047b11be0c71319e7224a
gaspadat <mailpakgasing@gmail.com>
2020-06-19 23:27:14 +0700

    fix cors origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ server.start({
   port: process.env.PORT || 4000,
   cors: {
     credentials: true,
-    origin: ['http://localhost:3000', 'https://rakyat62.netlify.com'],
+    origin: ['http://localhost:3000', 'https://rakyatapi.gaspadat.dev/ '],
   },
 }, (info) => {
   console.log('The server is up!', info);
